d18aa7a2b2db207d2b4e86fd44ccfc4ea1af8439
Eduardo <duduprado294@gmail.com>
2023-02-16 20:12:24 -0300

    Corrige outro erro de lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,7 +4,7 @@ def study_schedule(permanence_period, target_time):
     number_of_students = 0
     for period in permanence_period:
         if period[0] and period[1] and type(period[0]) == int\
-            and type(period[1]) == int:
+                and type(period[1]) == int:
             if period[0] <= target_time <= period[1]:
                 number_of_students += 1
         else:
