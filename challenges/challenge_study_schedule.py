eeebe089524baae3ea3816ccc09ba0543d017637
Eduardo <duduprado294@gmail.com>
2023-02-16 19:54:18 -0300

    Faz req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(target_time) != int and not target_time:
+        return None
+    number_of_students = 0
+    for period in permanence_period:
+        if period[0] and period[1] and type(period[0]) == int and type(period[1]) == int:
+            if period[0] <= target_time <= period[1]:
+                number_of_students += 1
+        else:
+            return None
+    return number_of_students
+    
