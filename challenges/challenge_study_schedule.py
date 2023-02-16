c50f4c76364c8f0607529d97f4e246ba381db622
Eduardo <duduprado294@gmail.com>
2023-02-16 20:01:28 -0300

    Corrige erro de lit

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,7 +3,8 @@ def study_schedule(permanence_period, target_time):
         return None
     number_of_students = 0
     for period in permanence_period:
-        if period[0] and period[1] and type(period[0]) == int and type(period[1]) == int:
+        if period[0] and period[1] and type(period[0]) == int\
+            and type(period[1]) == int:
             if period[0] <= target_time <= period[1]:
                 number_of_students += 1
         else:
