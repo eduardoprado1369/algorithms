7f0bc0f85d0735be7146ba9927d6268a1053ca37
Eduardo <duduprado294@gmail.com>
2023-03-23 11:39:59 -0300

    Diminui a complexidade do código do req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,15 +1,11 @@
 def is_palindrome_iterative(word):
-    # print(word)
     if len(word) == 1:
         return True
     if not word:
         return False
-    for index in range(len(word)):
+    for index, letter in enumerate(word):
         high_index = len(word) - (index + 1)
         if index < high_index:
-            # print(index)
-            # print(word[index])
-            # print(high_index)
-            if word[index] != word[high_index]:
+            if letter != word[high_index]:
                 return False
     return True
