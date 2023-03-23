7e1b9ab1ab20a696e3c9ba662e079d481d859998
Eduardo <duduprado294@gmail.com>
2023-03-23 13:32:47 -0300

    Faz o req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,11 +1,10 @@
 def is_palindrome_iterative(word):
-    if len(word) == 1:
-        return True
     if not word:
         return False
-    for index, letter in enumerate(word):
+    if len(word) == 1:
+        return True
+    for index, _ in enumerate(word):
         high_index = len(word) - (index + 1)
-        if index < high_index:
-            if letter != word[high_index]:
-                return False
+        if index < high_index and word[index] != word[high_index]:
+            return False
     return True
