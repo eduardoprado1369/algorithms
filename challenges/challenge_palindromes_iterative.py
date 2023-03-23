e34e1d2cb92dade9d3d4400f0c4bdd595851953b
Eduardo <duduprado294@gmail.com>
2023-03-23 11:34:34 -0300

    Faz o req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,15 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # print(word)
+    if len(word) == 1:
+        return True
+    if not word:
+        return False
+    for index in range(len(word)):
+        high_index = len(word) - (index + 1)
+        if index < high_index:
+            # print(index)
+            # print(word[index])
+            # print(high_index)
+            if word[index] != word[high_index]:
+                return False
+    return True
